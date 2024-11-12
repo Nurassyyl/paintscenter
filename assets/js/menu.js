eddf9d63d94c170c09ad26d98ad669f21f461662
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2024-11-12 14:50:44 +0500

    m

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,13 +1,16 @@
 const burgerBtn = document.querySelector(".burger");
 const burgerBtnClose = document.querySelector(".burger-close");
 const menu = document.querySelector(".nav-main-mobile");
+const body = document.body;
 
 burgerBtn.addEventListener('click', () => {
   menu.classList.add("active");
+  body.classList.add("hidden");
 })
 
 burgerBtnClose.addEventListener('click', () => {
   menu.classList.remove("active");
+  body.classList.remove("hidden");
 })
 
 // submenu
